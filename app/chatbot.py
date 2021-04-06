010e3043e24e15d4c573588c43eb0174cf7c591b
demetrio_bknp <demetrio@bknp.com.br>
2021-04-05 22:21:41 -0300

    Added checking if the share's value could be numeric, a real price

--- app/chatbot.py
+++ app/chatbot.py
@@ -5,10 +5,8 @@ def get_stock(data):
         try:
             share = data.split('/stock=')[1].strip()
             url = f'https://stooq.com/q/l/?s={share.lower()}&f=sd2t2ohlcv&h&e=csv'
-            print(url)
             df = pd.read_csv(url)
-            print(df)
-            price = df['Close'].values[0]
+            price = float(df['Close'].values[0])
             msg = f"{share.upper()} quote is ${price} per share"
         except Exception as e:
             print(e)
