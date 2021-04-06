8130ba661422ea00fe952810c6fa25590b1bde04
demetrio_bknp <demetrio@bknp.com.br>
2021-04-05 23:39:02 -0300

    bug fixing

--- setup-env.sh
+++ setup-env.sh
@@ -3,4 +3,4 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 python -m virtualenv venv
 source venv/Scripts/activate
 pip install -r requirements.txt -U
-export FLASK_APP=hello
+export FLASK_APP=chatroom.py
