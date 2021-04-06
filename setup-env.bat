08b86dc139afc62293c0a323b56bee4644769e08
demetrio_bknp <demetrio@bknp.com.br>
2021-04-06 00:05:16 -0300

    Bug Fix in settings and starting app

--- setup-env.bat
+++ setup-env.bat
@@ -6,3 +6,8 @@ python -m virtualenv venv
 call venv\Scripts\activate
 pip install -r requirements.txt -U
 set FLASK_APP=chatroom.py
+flask db init
+flask db migrate -m "users table"
+flask db upgrade
+flask db migrate -m "posts table"
+flask db upgrade
--- setup-env.sh
+++ setup-env.sh
@@ -4,3 +4,8 @@ python -m virtualenv venv
 source venv/Scripts/activate
 pip install -r requirements.txt -U
 export FLASK_APP=chatroom.py
+flask db init
+flask db migrate -m "users table"
+flask db upgrade
+flask db migrate -m "posts table"
+flask db upgrade
